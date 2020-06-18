0b5306b2108da4361f4e672d9cf9b8fe5d138ff6
= <44825869+lawalyusuf@users.noreply.github.com>
2020-06-18 22:37:11 +0100

    Create max_num_function_with_3_value.py

---
+++ max_num_function_with_3_value.py
@@ -0,0 +1,11 @@
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        print('The max number is ' + str(num1))
+        
+    elif num2 >= num1 and num2 >= num3:
+        print('The max number is ' + str(num2))
+        
+    else:
+        print('The max number is '  + str(num3))
+        
+    
