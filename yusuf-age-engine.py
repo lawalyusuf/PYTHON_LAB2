5f9a158eb7a55014866864c066b810009c6423f7
= <44825869+lawalyusuf@users.noreply.github.com>
2020-06-18 22:38:30 +0100

    Create yusuf-age-engine.py

---
+++ yusuf-age-engine.py
@@ -0,0 +1,30 @@
+from turtle import *
+write("WELCOME TO LAWAL O. YUSUF'S AGE ENGINE", move=("True"), align=("center"), font=("magneto",15,"normal"))
+
+
+print("welcome to L.O.Yusuf's age machine,ICT Instructor at GIT Solution & Resource Limited. Thank for using my machine....")
+print("lets see how long you have lived in years,days,minutes and seconds.")
+nickname =input("nickname:")
+print("your country")
+country =input("country: ")
+print("You were born in",country,",Right!")
+print(nickname,"was born in",country,",Nice one!")
+print("click enter to begin")
+start =input("start")
+print("let start with your name")
+name =input("name: ")
+print("now enter your age")
+age =int(input("age:"))
+years=(66-age) + 1952
+days = age * 365
+minutes = age * 525948
+seconds = age * 31556926
+print(name,"was born in", years,",")
+print(name,"has been alive for", days,"days", minutes, "minutes and", seconds, "seconds!IWULLNP in good health and God's Blessing!")
+print("click ENTER to stop program, If any correction or links to help me improve  Please inbox me: bwitlawalyusuf@gmail.com,")
+stop =input("stop")
+write("GOOD BYE", move=("True"), align=("center"), font=("roman",30,"normal"))
+
+
+         
+      
